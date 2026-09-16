3d5309c40d3a68d0241685f0949dd196b3f13186
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2026-09-16 11:10:57 +0300

    feat: add CORS support and fetch products in HomePage component

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import productRoutes from './routes/product.route.js'
@@ -8,6 +9,7 @@ dotenv.config()
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors())
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,35 +1,33 @@
 import {Heading, Flex, VStack} from "@chakra-ui/react"
 import ProductCard from "../components/ProductCard";
+import { useEffect, useState } from "react";
 
 const HomePage = () => {
-  const products =[
-    {
-    id: 1,
-    name: "Laptop",
-    price: 999,
-    image: "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    id: 2,
-    name: "Headphones",
-    price: 299,
-    image: "https://plus.unsplash.com/premium_photo-1679513691474-73102089c117?q=80&w=1113&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    id: 3,
-    name: "Mouse",
-    price: 129,
-    image: "https://images.unsplash.com/photo-1615663245857-ac93bb7c39e7?q=80&w=765&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  ];
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null)
+
+
+  useEffect(()=>{
+    fetch("http://localhost:5000/api/products")
+    .then((response)=> response.json())
+    .then((data) => {
+
+      setProducts(data.data);
+    })
+    .catch((error)=> {
+      setError(error.message);
+    });
+
+  }, [])
 
   return (
     <VStack spacing={8}>
       <Heading size="2xl">Current Products 🚀</Heading>
       <Flex gap="6" wrap="wrap">
+        {error && <p>Error: {error}</p>}
         {products.map((product) => (
           <ProductCard
-            key={product.id}
+            key={product._id}
             product={product}
           />
         ))}
